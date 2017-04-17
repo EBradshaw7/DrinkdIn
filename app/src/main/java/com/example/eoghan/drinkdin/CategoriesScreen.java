6aad408094761f2937f7cf719a25e096f3b27671
Eoghan <b00072106@student.itb.ie>
2017-04-17 23:23:29 +0100

    swapped buttons for listview

--- app/src/main/java/com/example/eoghan/drinkdin/CategoriesScreen.java
+++ app/src/main/java/com/example/eoghan/drinkdin/CategoriesScreen.java
@@ -4,29 +4,54 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 /**
  * Created by Eoghan on 07/12/2016.
  */
 
-public class CategoriesScreen extends AppCompatActivity implements View.OnClickListener {
+public class CategoriesScreen extends AppCompatActivity{
+
+    String[] menuListCat = {
+            "Craft",
+            "Guinness",
+            "Whiskey",
+            "Cocktail"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cat);
 
-        Button craftButton = (Button) findViewById(R.id.craftBtn);
-        craftButton.setOnClickListener(this);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                R.layout.activity_list, menuListCat);
+
+        ListView list = (ListView) findViewById(R.id.listMenuCat);
+        list.setAdapter(adapter);
+
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                switch(position){
+                    case 0:  Intent phouseActivity = new Intent(CategoriesScreen.this, phouseMap.class);
+                        startActivity(phouseActivity);
+                        break;
+
+
+
+                }
+
+            }
+            @SuppressWarnings("unused")
+            public void onClick(View v){
+            }
+        });
+
+
 
     }
-
-        public void onClick (View v){
-            //Log.i("clicks", "You Clicked B1");
-            Intent i = new Intent(CategoriesScreen.this, CraftList.class);
-            startActivity(i);
-
-
-        }
-    }
+}
